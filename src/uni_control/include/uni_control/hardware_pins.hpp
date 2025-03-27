bfccd9df6982c31ada1b6d60f92c1ffb502a83b5
olegaobini <obiniolega@gmail.com>
2025-03-27 20:53:11 +0000

    some changes

---
+++ src/uni_control/include/uni_control/hardware_pins.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace hw_pins {
+
+// Motor 1
+constexpr int MOTOR1_PWM_PIN = 18;
+constexpr int MOTOR1_DIR_PIN = 15;
+constexpr int MOTOR1_ENC_A_PIN = 23;
+constexpr int MOTOR1_ENC_B_PIN = 24;
+
+// Motor 2
+constexpr int MOTOR2_PWM_PIN = 12;
+constexpr int MOTOR2_DIR_PIN = 16;
+constexpr int MOTOR2_ENC_A_PIN = 20;
+constexpr int MOTOR2_ENC_B_PIN = 21;
+
+// Motor 3
+constexpr int MOTOR2_PWM_PIN = 13;
+constexpr int MOTOR2_DIR_PIN = 6;
+constexpr int MOTOR2_ENC_A_PIN = 0;
+constexpr int MOTOR2_ENC_B_PIN = 5;
+
+//// LiDAR, buzzer, sensors, etc.
+//constexpr int BUZZER_PIN = 4;
+//constexpr int LIDAR_ENABLE_PIN = 10;
+
+}
